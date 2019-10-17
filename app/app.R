51094bb1878995a463f7e2e12f30546ce97db117
nderon <nderon@andrew.cmu.edu>
2019-10-17 11:12:45 -0400

    Moved data into app folder; began construction of datatable. Not currently working

--- app/app.R
+++ app/app.R
@@ -4,6 +4,7 @@ library(leaflet)
 library(sp)
 library(sf)
 library(rgdal)
+library(DT)
 
 ##Data prep
 blotter <- read.csv("Blotter_Data_Archive.csv") #Read blotter data
@@ -11,7 +12,7 @@ blotter$INCIDENTTIME <- strptime(x = as.character(blotter$INCIDENTTIME), #Conver
                                  format = "%Y-%m-%dT%H:%M:%S")
 blotter$type <- cut(blotter$HIERARCHY, c(-Inf, 9, 98, Inf), labels = c("Type 1 - Major Crime", "Type 2 - Minor Crime", "No Data or None")) #Convert hierarchy to bucketed factors
 blotter_subset <- blotter[blotter$INCIDENTTIME >= "2018-01-01" & blotter$INCIDENTTIME <= "2018-12-31" & blotter$X <= -78 & blotter$Y >= 39 & !is.na(blotter$type),]
-blotter_subset <- blotter_subset[sample(1:nrow(blotter_subset), 25),]
+blotter_subset <- blotter_subset[sample(1:nrow(blotter_subset), 200),]
 historic <- readOGR("City_Designated_Historic_Districts.geojson.json") #read historic district dat
 cc_districts <- readOGR("City_Council_Districts.geojson") #read city council district data
 
@@ -25,7 +26,10 @@ ui <- fluidPage(
         mainPanel(
             
             tabsetPanel(
-                tabPanel()
+                tabPanel("Map"),
+                tabPanel("Time Trends"),
+                tabPanel("Data", dataTableOutput(outputId = "DT"))
+                
             )
         )
     )
@@ -33,7 +37,11 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
+    
+    output$DT <- renderDataTable({    #Datatable code
+        DT::datatable(blotter_subset(), options = list(scrollY = "300px", scrollX = T))
+    })
+    
 }
 
 # Run the application 
